9f894eb5ca8001defa66774c42de01bad91ecdf8
Régis JAMBOIS <regis_jambois@mac13p.local>
2018-10-27 08:07:30 +0200

    INIT PRJ

---
+++ app/src/main/java/net/jbsw/simulateurdemprunt/MainActivity.java
@@ -0,0 +1,13 @@
+package net.jbsw.simulateurdemprunt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
